942e26b3ecf111e16cfccaba14248b83d4d65750
Moura196 <gmendesmoura@gmail.com>
2024-11-25 11:35:27 -0300

    Commit inicial.

---
+++ pedidos/src/test/java/api/produtos/pedidos/PedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package api.produtos.pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
